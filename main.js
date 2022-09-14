e8756f4926d2cadd518e8ca746e95c3457c53129
sebastian rubilar <108100307+rub1lar@users.noreply.github.com>
2022-09-14 19:56:28 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -25,14 +25,20 @@ function capturar() {
     capturarEmail != "" &&
     capturarContraseña != ""
   ) {
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title:
+        "Listo!  " +
+        capturarNombre +
+        " Quedaste registrado como: " +
+        capturarUsuario +
+        " te llegara un email de confirmacion a:  " +
+        capturarEmail,
+      showConfirmButton: true,
+      timer: 4000,
+    });
     console.log(nuevoRegistrado);
-    document.getElementById("regis").innerHTML +=
-      "<h3>Listo!" +
-      capturarNombre +
-      " Quedaste Registrado Como : " +
-      capturarUsuario +
-      " te llegara un email de confirmacion a " +
-      capturarEmail;
     ArrayRegistrados.push(nuevoRegistrado);
     guardarLocalStorage();
   }
@@ -58,18 +64,23 @@ function Loggearse() {
     (registro) =>
       registro.usuario == usuarioL && registro.contraseña == contraseña
   );
-  //si el usuario y la contraseña son iguales a los de uno del objeto del array devolver true//
-  if (UsuarioLogeado) {
-    document.getElementById("logeo").innerHTML += "La Contraseña Es correcta";
-  } else {
-    document.getElementById("logeo").innerHTML +=
-      "Contraseña incorrecta, intente nuevamente";
+  if (UsuarioLogeado && usuarioL != "" && contraseña != "") {
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Contraseña Correcta",
+      showConfirmButton: true,
+      timer: 4000,
+    });
+  } else if (UsuarioLogeado == false || (usuarioL != "" && contraseña != "")) {
+    Swal.fire({
+      position: "center",
+      icon: "error",
+      title: "Contraseña Incorrecta",
+      timer: 2000,
+    });
   }
 }
-// aca hice una funcion Presupuesto donde elige por medio de botones el tipo de cartel, luego ingresaa en el imput number la cantidad de metros
-//que necesita de cartel, si lo campos estan vacios no hace nada, asi como si es menor a 0.
-// depende el tipo que elija hace una cuenta diferente por la cantidad de metros ingresada, tambien verifica si esta registrado y le suma un 10 %
-//de descuento al precio final
 
 //hago clase para los objetos, que seran 3 tipos
 //diferentes de carteles, front- back-ligth y vinilo.
@@ -89,9 +100,7 @@ let Vinilo = new cartel("vinilo", 800, true, "vinilo");
 //creo array //
 const carteles = [];
 /// agrego con push cartelFront, cartelBack, Vinilo//
-carteles.push(cartelFront);
-carteles.push(cartelBack);
-carteles.push(Vinilo);
+carteles.push(cartelFront, cartelBack, Vinilo);
 console.log(carteles);
 /// filtro del array creado los carteles que son de lona//
 let cartelesDeLona = carteles.filter((cartel) => cartel.categoria == "lona");
@@ -111,61 +120,176 @@ enterr.addEventListener("keyup", (event) => {
 let front = false;
 let backl = false;
 let fro = document.getElementById("front");
+let bk = document.getElementById("backl");
 fro.addEventListener("click", () => {
+  fro.style.backgroundColor = "green";
+  bk.style.backgroundColor = "";
+
   front = true;
   backl = false;
 });
-document.getElementById("backl").addEventListener("click", () => {
+bk.addEventListener("click", () => {
+  fro.style.backgroundColor = "";
+  bk.style.backgroundColor = "green";
+
   backl = true;
   front = false;
 });
 
+// aca hice una funcion Presupuesto donde elige por medio de botones el tipo de cartel, luego ingresaa en el imput number la cantidad de metros
+//que necesita de cartel, si lo campos estan vacios no hace nada, asi como si es menor a 0.
+// depende el tipo que elija hace una cuenta diferente por la cantidad de metros ingresada, tambien verifica si esta registrado y le suma un 10 %
+//de descuento al precio final
+let arrayFinal = [];
 function Presupuesto() {
+  let presufinish= document.getElementById(`presuFinal`);
+
   let imputNumber = document.getElementById("number").value;
   let resultadoF = imputNumber * cartelFront.precio;
   let resultadoB = imputNumber * cartelBack.precio;
   let descuentoB = resultadoB * 0.9;
   let descuentoF = resultadoF * 0.9;
   if (front == true && UsuarioLogeado && imputNumber != "" && imputNumber > 0) {
-    document.getElementById("cuerpoPresupuesto").innerHTML +=
-      " <tr><td>" +
-      "CARTEL FRONT" +
-      "</td><td>" +
-      imputNumber +
-      "</td><td> $" +
-      descuentoF +
-      "</td></tr> ";
+    document.getElementById("cuerpoPresupuesto").innerHTML += `
+<tr><td>
+<p>CARTEL FRONT</p>
+</td>
+<td>
+<p>${imputNumber}</p>
+</td>
+<td>
+<p>${descuentoF}</p>
+</td>
+<td> <button  onclick ="deletear(this);"> borrar</button> </td>
+</tr>
+`;
+
+    arrayFinal.push(descuentoF);
+
+    const resultado = arrayFinal.reduce((acc, item) => {
+      return (acc = acc + item);
+    });
+
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "El Total De Tu Presupuesto Para Cartel Front Es: $ " + descuentoF,
+      showConfirmButton: true,
+      timer: 6000,
+    });
+
+/*     document.removeChild(presufinish);/*  
+
+   /*  presufinish.replaceChild(`<p>${resultado}</p>`,presufinish.children[0]); */
+    presufinish.innerHTML += `<p>${resultado}</p>`; 
+    
+
+
   } else if (front == true && imputNumber != "" && imputNumber > 0) {
-    document.getElementById("cuerpoPresupuesto").innerHTML +=
-      " <tr><td>" +
-      "CARTEL FRONT" +
-      "</td><td>" +
-      imputNumber +
-      "</td><td> $" +
-      resultadoF +
-      "</td></tr> ";
+ document.getElementById("cuerpoPresupuesto").innerHTML += `
+    
+<tr><td>
+<p>CARTEL FRONT</p>
+</td>
+<td>
+<p>${imputNumber}</p>
+</td>
+<td>
+<p>${resultadoF}</p>
+</td>
+<td> <button  onclick ="deletear(this);"> borrar</button> </td>
+</tr>
+`;
+    arrayFinal.push(resultadoF);
+    
+    const resultado = arrayFinal.reduce((acc, item) => {
+      acc = parseInt(acc) + parseInt(item);
+      return acc;
+    });
+    
+  /*   pres.replaceChild(resultado,press.children[0]); */
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "El Total De Tu Presupuesto Para Cartel Front Es: $ " + resultadoF,
+      showConfirmButton: true,
+      timer: 6000,
+    });
+
+/* 
+    presufinish.children[0].replaceWith(resultado); */
+/*     document.removeChild(presufinish); */
+    /* presufinish.replaceChild(`<p>${resultado}</p>`,presufinish.children[0]); */
+   let y= presufinish.innerHTML += `<p>${resultado}</p>`;
   } else if (
     backl == true &&
     UsuarioLogeado &&
     imputNumber != "" &&
     imputNumber > 0
   ) {
-    document.getElementById("cuerpoPresupuesto").innerHTML +=
-      " <tr><td>" +
-      "CARTEL BACK" +
-      "</td><td>" +
-      imputNumber +
-      "</td><td> $" +
-      descuentoB +
-      "</td></tr> ";
+    document.getElementById("cuerpoPresupuesto").innerHTML += `
+    <tr><td>
+    <p>CARTEL BACK-LIGHT</p>
+    </td>
+    <td>
+    <p>${imputNumber}</p>
+    </td>
+    <td>
+    <p>${descuentoB}</p>
+    </td>
+    <td> <button  onclick ="deletear(this);"> borrar</button> </td>
+    </tr>
+    `;
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title:
+        "El Total De Tu Presupuesto Para Cartel Back Light Es: $ " + descuentoB,
+      showConfirmButton: true,
+      timer: 6000,
+    });
   } else if (backl == true && imputNumber != "" && imputNumber > 0) {
-    document.getElementById("cuerpoPresupuesto").innerHTML +=
-      " <tr><td>" +
-      "CARTEL BACK" +
-      "</td><td>" +
-      imputNumber +
-      "</td><td> $" +
-      resultadoB +
-      "</td></tr> ";
+    document.getElementById("cuerpoPresupuesto").innerHTML += `
+    <tr><td>
+    <p>CARTEL BACK-LIGHT</p>
+    </td>
+    <td>
+    <p>${imputNumber}</p>
+    </td>
+    <td>
+    <p>${resultadoB}</p>
+    </td>
+    <td> <button  onclick ="deletear(this);"> borrar</button> </td>
+    </tr>
+    `;
+
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title:
+        "El Total De Tu Presupuesto Para Cartel Back Light Es: $ " + resultadoB,
+      showConfirmButton: true,
+      timer: 6000,
+    });
   }
+
+/*   presufinish.remove("p"[1]); */
+
 }
+
+function del(el) {
+  const item = el.parentElement;
+  item.remove();
+}
+
+
+function deletear(el) {
+  const item = el.parentElement.parentElement;
+  item.remove();
+
+}
+
+/* const resultado = arrayFinal.reduce((acc , item ) => {
+return acc = acc + item;
+})
+ */
